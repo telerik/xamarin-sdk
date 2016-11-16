f60f584f2f8cf064331e98ebb662843e08328cd9
Nikolay Diyanov <diyanov@progress.com>
2016-11-16 19:53:37 +0200

    VisualStudioforMac

---
+++ VisualStudioforMacGStarted/Test/TestPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Telerik.XamarinForms.Input;
+using Xamarin.Forms;
+
+namespace Test
+{
+	public partial class TestPage : ContentPage
+	{
+		public TestPage()
+		{
+			InitializeComponent();
+			this.Content = new RadCalendar();
+		}
+	}
+}
---
+++ VisualStudioforMacGStarted/iOS/AppDelegate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Foundation;
+using Telerik.XamarinForms.Common.iOS;
+using UIKit;
+using Xamarin.Forms;
+
+[assembly: ExportRenderer(typeof(Telerik.XamarinForms.Input.RadCalendar), typeof(Telerik.XamarinForms.InputRenderer.iOS.CalendarRenderer))]
+
+namespace Test.iOS
+{
+	[Register("AppDelegate")]
+	public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+	{
+		public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+		{
+			new Telerik.XamarinForms.InputRenderer.iOS.CalendarRenderer();
+
+			global::Xamarin.Forms.Forms.Init();
+			TelerikForms.Init();
+
+			LoadApplication(new App());
+
+			return base.FinishedLaunching(app, options);
+		}
+	}
+}
